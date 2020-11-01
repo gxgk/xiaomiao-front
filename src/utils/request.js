311fcccbe6bdf080685b68f86acce30edadb7ced
WeijiangChen <ourweijiang@gmail.com>
2020-11-01 23:17:41 +0800

    修复登录无限跳转bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -27,9 +27,9 @@ service.interceptors.response.use(
      */
     const res = response.data
     if (Number(res.c) === 302) {
-      const redirectUrl = encodeURIComponent(window.location.href)
+      const redirectUrl = `${res.d.callback}?callback_url=${encodeURIComponent(window.location.href)}`
       window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${res.d.appid}` +
-      `&redirect_uri=${redirectUrl}&response_type=code&scope=${res.d.scope}&component_appid=${res.d.component_appid}&connect_redirect=1`
+      `&redirect_uri=${encodeURIComponent(redirectUrl)}&response_type=code&scope=${res.d.scope}&component_appid=${res.d.component_appid}&connect_redirect=1`
       return Promise.reject(new Error(response.data.m))
     } else if (Number(res.c) !== 0) {
       Toast.fail(res.m)
